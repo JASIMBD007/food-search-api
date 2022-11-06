4b6bc6e4e95127b100a0cc7a6a5228809081bf81
Jasim Uddin <jasim.ice.iu@gmail.com>
2022-11-06 12:36:17 +0100

    search repetation fixed

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ const searchFood = () => {
     const searchText = searchField.value;
     // console.log(searchText);
     searchField.value = '';
+    searchField.textContent = '';
+    if (searchText == '') {
+        console.log('Please write something to display');
+    } else {
+    }
+    // clear data
+
+
+    //load data    
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     // console.log(url);
     fetch(url)
@@ -13,12 +22,13 @@ const searchFood = () => {
 const displaySearchResult = meals => {
     // console.log(meals);
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     meals.forEach(meal => {
         console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div onclick="loadMealDetails(${meal.idMeal})" class="card">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
@@ -28,3 +38,28 @@ const displaySearchResult = meals => {
         searchResult.appendChild(div);
     });
 }
+const loadMealDetails = async mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => displayMealDetails(data.meals[0]));
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMealDetails(data.meals[0]);
+};
+const displayMealDetails = meal => {
+    // console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img class="w-50 mx-auto" src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions}</p>
+                <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+            </div>
+    `;
+    mealDetails.appendChild(div);
+};
